20f3a38fd941521c323f2828098b097213d3bf8d
zoe <zfowler3@gatech.edu>
2025-03-20 11:41:55 -0400

    Update to repo: partitioning clients by patients for medical datasets

--- train_tools/preprocessing/olives/loader.py
+++ train_tools/preprocessing/olives/loader.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import numpy as np
 
 def get_patient_id(spreadsheet_root, mode='tr'):
+    '''
+    Returns unique patient IDs in given train/test split
+    '''
     spreadsheet_root = spreadsheet_root + '/spreadsheets/'
     if mode == 'tr':
         sheet = pd.read_csv(spreadsheet_root + 'prime_trex_compressed.csv')
     else:
         sheet = pd.read_csv(spreadsheet_root + 'prime_trex_compressed_new.csv')
+    ids = sheet['Patient_ID'].to_numpy()
+
+    return np.unique(ids)
