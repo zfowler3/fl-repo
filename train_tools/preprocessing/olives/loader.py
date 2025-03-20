8da2c2fc338cc0804b7d5f376baa3cf5b1b63716
zoe <zfowler3@gatech.edu>
2025-03-20 11:48:09 -0400

    Update to repo: partitioning clients by patients for medical datasets

--- train_tools/preprocessing/olives/loader.py
+++ train_tools/preprocessing/olives/loader.py
@@ -13,3 +13,24 @@ def get_patient_id(spreadsheet_root, mode='tr'):
     ids = sheet['Patient_ID'].to_numpy()
 
     return np.unique(ids)
+
+def get_patient_ids_by_visit(spreadsheet_root, max_val, mode='tr'):
+    '''
+    Returns unique patient IDs in given train/test split, where only patients have
+    certain #s of visits are considered (max_val)
+    '''
+    spreadsheet_root = spreadsheet_root + '/spreadsheets/'
+    if mode == 'tr':
+        sheet = pd.read_csv(spreadsheet_root + 'prime_trex_compressed.csv')
+    else:
+        sheet = pd.read_csv(spreadsheet_root + 'prime_trex_compressed_new.csv')
+    ids = sheet['Patient_ID'].to_numpy()
+    unique_ids = np.unique(ids)
+    new_ids = []
+    for i in unique_ids:
+        subsheet = sheet[sheet['Patient_ID'] == i]
+        max_visit = subsheet['Visit'].max()
+        # if patient has at least 'max_val' number of visits, include it
+        if max_visit >= max_val:
+            new_ids.append(i)
+    return np.array(new_ids)
